753a4803cf844e0d37d32cbcd6891d31381e716e
DESKTOP-5PHL67U\Carlos <csi23-cmejero@colegioaltair.net>
2024-03-14 12:33:25 +0100

    no message

---
+++ edu.LM.Ejercicio1403/src/main/webapp/java.js
@@ -0,0 +1,15 @@
+
+
+
+function conversion() {
+	let num = prompt("Introduzca un numero: ");
+	if (num > 0) {
+
+
+	}
+	else {
+		num *= -1;
+	}
+	document.getElementById("texto").innerHTML = (num);
+
+}
